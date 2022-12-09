310669009a24f3e16e6ad2ae1e59c1dcf7ab7488
Evgeniia Zorina <evgeniia.zorina@indriver.com>
2022-12-09 23:05:01 +0200

    Выполнила задание №2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -8,4 +8,10 @@ public class MainClassTest extends MainClass {
     {
         Assert.assertTrue("Метод getLocalNumber не возвращает число 14", getLocalNumber() == 14);
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        Assert.assertTrue("Метод getClassNumber возвращает число меньше 45", getClassNumber() > 45);
+    }
 }
